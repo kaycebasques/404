2699df31af3b1c80d43b49affce471599e4b045a
Kayce Basques <kayce@google.com>
2022-12-17 13:47:58 -0800

    Start logic and structure

--- src/scrape.py
+++ src/scrape.py
@@ -1,1 +1,14 @@
-print('Hello, scrape.py!')
+from playwright.sync_api import sync_playwright
+
+def main():
+  with sync_playwright as p:
+    browser = p.chromium.launch()
+    page = browser.new_page()
+    url = 'https://fuchsia.dev'
+    page.goto(url)
+    anchor_nodes = page.query_selector_all('a')
+    for anchor_node in anchor_nodes:
+      print(anchor_node.get_attribute('href'))
+
+if __name__ == '__main__':
+  main()
