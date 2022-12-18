0a6cfa873979a5560224cd17f5ef4f94b26f762d
Kayce Basques <kayce@google.com>
2022-12-17 19:29:58 -0800

    Continue MVP logic

--- src/main.py
+++ src/main.py
@@ -1,36 +1,76 @@
 import lib
 from playwright.sync_api import sync_playwright
 
-# interval val in config can determine how frequently data is written
-# site.json can store site info
-# metadata.json can store info about each url
+# Remember that the goal here is to catch 404s so you
+# don't want to do too much processing.
 
-def scrape_urls(page):
-  urls = []
+# Unused ATM.
+# TODO: Create url.py and move there?
+# def normalize_href(href, start_url):
+#   # TODO: Handle case where the URL points back to the current page.
+#   if href.startswith('http'):
+#     return href
+#   elif href.startswith('//'):
+#     # https://stackoverflow.com/a/9646435/1669860
+#     return 'TODO(//)'
+#   elif href.startswith('/'):
+#     return '{}{}'.format(start_url, href)
+#   elif href.startswith('#'):
+#     return 'TODO(#)'
+#   else:
+#     print('Unexpected href:', href)
+#     return 'TODO({})'.format(href)
+
+# Don't get too fancy with processing hrefs?
+# What if we just use Playwright's click feature instead?
+# def scrape(start_url, page):
+#   # TODO: Add start_url to site.json?
+#   site_data = lib.read_json('data/site.json')
+#   todo = site_data['todo']
+#   if len(todo) == 0:
+#     return
+#   target_url = todo.pop(0)
+#   page.goto(target_url, wait_until='domcontentloaded')
+#   final_target_url = page.url
+#   anchor_nodes = page.query_selector_all('a')
+#   site_data[target_url] = {}
+#   for anchor_node in anchor_nodes:
+#     href = anchor_node.get_attribute('href')
+#     # TODO: site_data[target_url]['hrefs'][href] instead?
+#     # For example we should maybe store that final_target_url value...
+#     site_data[target_url][href] = {}
+#   # TODO: Need to update the todo list.
+#   # TODO: URL metadata. Maybe create url.py?
+#   lib.write_json('data/site.json', site_data)
+
+def scrape(start_url, page):
+  # TODO: Add start_url to site.json?
+  site_data = lib.read_json('data/site.json')
+  todo = site_data['todo']
+  if len(todo) == 0:
+    return
+  target_url = todo.pop(0)
+  site_data[target_url] = {}
+  page.goto(target_url, wait_until='domcontentloaded')
+  final_target_url = page.url
   anchor_nodes = page.query_selector_all('a')
   for anchor_node in anchor_nodes:
-    # TODO: Normalize URLs. Some might just be paths.
-    # Should return both raw href and normalized URL.
-    urls.append(anchor_node.get_attribute('href'))
-  return urls
+    anchor_node.click(modifiers=['Control'])
+  lib.write_json('data/site.json', site_data)
 
-def maybe_write():
-  # divide current by interval and if remainder is 0 then write
-  return 'TODO'
-
-# I think this logic is all wrong. It's easiest to reason about
-# if you write after each page. Even if that is tough on storage.
 def main():
   config = lib.read_json('data/config.json')
+  start_url = config['start_url']
+  batch_size = config['batch_size']
   site_data = lib.read_json('data/site.json')
-  interval = config['interval'] # unused interval logic
-  urls_scraped = 0 # unused interval logic
+  if 'todo' not in site_data:
+    site_data['todo'] = [start_url]
+    lib.write_json('data/site.json', site_data)
   with sync_playwright() as synchronous_playwright:
-    browser = synchronous_playwright.chromium.launch()
+    browser = synchronous_playwright.chromium.launch(headless=False)
     page = browser.new_page()
-    current_url = config['entrypoint']
-    page.goto(current_url)
-    urls_found_on_page = scrape_urls(page)
+    for unused in range(batch_size):
+      scrape(start_url, page)
 
 if __name__ == '__main__':
   main()
