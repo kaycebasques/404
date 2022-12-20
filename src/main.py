9e800bc964702976f61f016c0532e2a8f421ee2e
Kayce Basques <kayce@google.com>
2022-12-19 16:20:39 -0800

    Update scraping logic

--- src/main.py
+++ src/main.py
@@ -1,76 +1,26 @@
 import lib
 from playwright.sync_api import sync_playwright
 
-# Remember that the goal here is to catch 404s so you
-# don't want to do too much processing.
-
-# Unused ATM.
-# TODO: Create url.py and move there?
-# def normalize_href(href, start_url):
-#   # TODO: Handle case where the URL points back to the current page.
-#   if href.startswith('http'):
-#     return href
-#   elif href.startswith('//'):
-#     # https://stackoverflow.com/a/9646435/1669860
-#     return 'TODO(//)'
-#   elif href.startswith('/'):
-#     return '{}{}'.format(start_url, href)
-#   elif href.startswith('#'):
-#     return 'TODO(#)'
-#   else:
-#     print('Unexpected href:', href)
-#     return 'TODO({})'.format(href)
-
-# Don't get too fancy with processing hrefs?
-# What if we just use Playwright's click feature instead?
-# def scrape(start_url, page):
-#   # TODO: Add start_url to site.json?
-#   site_data = lib.read_json('data/site.json')
-#   todo = site_data['todo']
-#   if len(todo) == 0:
-#     return
-#   target_url = todo.pop(0)
-#   page.goto(target_url, wait_until='domcontentloaded')
-#   final_target_url = page.url
-#   anchor_nodes = page.query_selector_all('a')
-#   site_data[target_url] = {}
-#   for anchor_node in anchor_nodes:
-#     href = anchor_node.get_attribute('href')
-#     # TODO: site_data[target_url]['hrefs'][href] instead?
-#     # For example we should maybe store that final_target_url value...
-#     site_data[target_url][href] = {}
-#   # TODO: Need to update the todo list.
-#   # TODO: URL metadata. Maybe create url.py?
-#   lib.write_json('data/site.json', site_data)
-
-def scrape(start_url, page):
-  # TODO: Add start_url to site.json?
-  site_data = lib.read_json('data/site.json')
-  todo = site_data['todo']
-  if len(todo) == 0:
-    return
-  target_url = todo.pop(0)
-  site_data[target_url] = {}
-  page.goto(target_url, wait_until='domcontentloaded')
-  final_target_url = page.url
+def scrape(context):
+  url = 'https://fuchsia.dev'
+  page = context.new_page()
+  page.goto(url, wait_until='domcontentloaded')
   anchor_nodes = page.query_selector_all('a')
   for anchor_node in anchor_nodes:
-    anchor_node.click(modifiers=['Control'])
-  lib.write_json('data/site.json', site_data)
+    if anchor_node.is_visible() == False:
+      continue
+    with context.expect_page() as child_page_info:
+      anchor_node.click(modifiers=['Control'])
+    child_page = child_page_info.value
+    child_page.close()
 
 def main():
-  config = lib.read_json('data/config.json')
-  start_url = config['start_url']
-  batch_size = config['batch_size']
-  site_data = lib.read_json('data/site.json')
-  if 'todo' not in site_data:
-    site_data['todo'] = [start_url]
-    lib.write_json('data/site.json', site_data)
   with sync_playwright() as synchronous_playwright:
     browser = synchronous_playwright.chromium.launch(headless=False)
-    page = browser.new_page()
-    for unused in range(batch_size):
-      scrape(start_url, page)
+    context = browser.new_context()
+    context.set_default_timeout(10000)
+    # I guess you can just keep loading data/site.json and running scrape until it's empty
+    scrape(context)
 
 if __name__ == '__main__':
   main()
